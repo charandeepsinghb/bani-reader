32ef9201581d9c36240ad4a8b677e5a2d189e4b8
Charandeep Singh <charandeeps81@gmail.com>
2023-08-27 08:09:17 +0530

    Added word spacing option and some ui improvements with Column width

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const MIN_FONT_SIZE = 1;
-const MAX_FONT_SIZE = 200;
+const MIN_SIZE = 1;
+const MAX_SIZE = 200;
 
 // Checks for font size in local storage
 let savedFontSize = localStorage.getItem('savedFontSize');
@@ -12,16 +12,19 @@ let optionsMenu;
 baniSection = document.getElementById("bani");
 optionsMenu = document.getElementById("popUp");
 
+let currentColumnWidth = document.getElementById("currentColumnWidth");
+let currentWordSpace = document.getElementById("currentWordSpace");
+
 // Get current font size of bani section
 currentFontSize = window.getComputedStyle(baniSection).getPropertyValue('font-size');
 
 // Number format font size
 numberFontSize = Number.parseFloat(currentFontSize.substring(0, currentFontSize.length - 2));
-setFontSizeInputValue(numberFontSize);
+setInputValue(numberFontSize, 'currentFontSize');
 
 if (savedFontSize) {
     changeFontSize(savedFontSize);
-    setFontSizeInputValue(savedFontSize);
+    setInputValue(savedFontSize, 'currentFontSize');
 }
 
 function changeBackgroundColor(value) {
@@ -42,22 +45,22 @@ function decreaseFontSize() {
 
 function increaesDecreaseFontSize(increaseDecreaseValue) {
     let newNumberFontSize = numberFontSize + increaseDecreaseValue;
-    if (!isBetween(MIN_FONT_SIZE, MAX_FONT_SIZE, numberFontSize)) {
+    if (!isBetween(MIN_SIZE, MAX_SIZE, numberFontSize)) {
         return;
     }
     numberFontSize = newNumberFontSize;
-    setFontSizeInputValue(numberFontSize);
+    setInputValue(numberFontSize, 'currentFontSize');
     baniSection.style.fontSize = numberFontSize + "px";
     saveFontSizeInStorage(numberFontSize);
 }
 
 // Change font size in input field
-function setFontSizeInputValue(value) {
-    document.getElementById("currentFontSize").value = Number.parseFloat(value).toFixed(1);
+function setInputValue(value, fieldId) {
+    document.getElementById(fieldId).value = Number.parseFloat(value).toFixed(1);
 }
 
 function changeFontSize(size) {
-    if (!isBetween(MIN_FONT_SIZE, MAX_FONT_SIZE, size)) {
+    if (!isBetween(MIN_SIZE, MAX_SIZE, size)) {
         return;
     }
     numberFontSize = Number.parseFloat(size);
@@ -77,10 +80,10 @@ function darkMode(isOn) {
 
 // Emit multiple events when called continuously
 let emitterInterv;
-function continuousEmitterStart(func) {
+function continuousEmitterStart(func, time) {
     emitterInterv = setInterval(() => {
         func()
-    }, 100);
+    }, time);
 }
 
 // Stops event emitting when called
@@ -134,4 +137,54 @@ function openCloseMenu() {
     } else {
         optionsMenu.style.display = 'none';
     }
+}
+
+function changeColumnWidth(size) {
+    if (!isBetween(MIN_SIZE, MAX_SIZE, size)) {
+        return;
+    }
+    baniSection.style.columnWidth = size + 'vw';
+}
+
+function changeInput(value, callback) {
+    callback(value);
+}
+
+function increaesDecreaseColumnWidth(increaseDecreaseValue) {
+    let newNumberColumnWidth = Number(currentColumnWidth.value) + increaseDecreaseValue;
+    if (!isBetween(MIN_SIZE, MAX_SIZE, newNumberColumnWidth)) {
+        setInputValue(newNumberColumnWidth, 'currentColumnWidth');
+        return;
+    }
+    setInputValue(newNumberColumnWidth, 'currentColumnWidth');
+    baniSection.style.columnWidth = newNumberColumnWidth + "vw";
+}
+
+function increaseColumnWidth() {
+    increaesDecreaseColumnWidth(5);
+}
+
+function decreaseColumnWidth() {
+    increaesDecreaseColumnWidth(-5);
+}
+
+
+currentWordSpace.value = Number.parseFloat(window.getComputedStyle(baniSection).getPropertyValue('word-spacing'));
+
+function increaesDecreaseWordSpace(increaseDecreaseValue) {
+    let newNumberWordSpace = Number.parseFloat(currentWordSpace.value) + increaseDecreaseValue;
+    if (!isBetween(0, MAX_SIZE, newNumberWordSpace)) {
+        setInputValue(newNumberWordSpace, 'currentWordSpace');
+        return;
+    }
+    setInputValue(newNumberWordSpace, 'currentWordSpace');
+    baniSection.style.wordSpacing = newNumberWordSpace + "px";
+}
+
+function increaseWordSpace() {
+    increaesDecreaseWordSpace(1);
+}
+
+function decreaseWordSpace() {
+    increaesDecreaseWordSpace(-1);
 }
